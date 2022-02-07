637f6ce53582e6824ebe770fbe0c2657602ba2a3
luucas-melo <lucasmelodos322@gmail.com>
2022-02-07 14:34:44 -0300

    add login label

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,31 +1,26 @@
-import { NextApiRequest } from 'next'
-import type { Session } from 'next-auth'
-import { getToken } from 'next-auth/jwt'
-import type { NextRequest } from 'next/server'
-import { NextResponse } from 'next/server'
+import { NextApiRequest } from 'next';
+import type { Session } from 'next-auth';
+import { getToken } from 'next-auth/jwt';
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 
-
-
-const routes: Record<string,string> = {
+const routes: Record<string, string> = {
   '/public': 'public',
   '/protected': 'protected',
-} 
+};
 
 export async function middleware(req: NextRequest) {
   if (routes[req.nextUrl.pathname]) {
-     
     const session = (await getToken({
       req: req as unknown as NextApiRequest,
       secret: 'secret-example',
-      secureCookie: false
-    })) as Session
-   
-    console.log('TEST',session)
-    if(!session && req.nextUrl.pathname !== '/public') {
-      return NextResponse.redirect('/public')
+      secureCookie: false,
+    })) as Session;
+
+    if (!session && req.nextUrl.pathname !== '/public') {
+      return NextResponse.redirect('/public');
     }
-  
   }
 
-  return NextResponse.next()
+  return NextResponse.next();
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,22 @@
 import type { NextPage } from 'next';
-import { Router, useRouter } from 'next/router';
-import { signIn, useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import { useSession } from 'next-auth/react';
 const Home: NextPage = () => {
   const router = useRouter();
-  const {data: session} = useSession()
+  const { data: session } = useSession();
   return (
     <div>
-      <h1>{session?.user?.name ? session?.user?.name + ' isLogged in' : 'User is not logged in'} </h1>
+      <h1>Login</h1>
+      <h1>
+        {session?.user?.name
+          ? session?.user?.name + ' isLogged in'
+          : 'User is not logged in'}{' '}
+      </h1>
       <h3>click in the button</h3>
-      <button style={{ backgroundColor: 'black', color: 'white' }} onClick={() => router.push('/protected')}>
+      <button
+        style={{ backgroundColor: 'black', color: 'white' }}
+        onClick={() => router.push('/protected')}
+      >
         Protected page
       </button>
     </div>
